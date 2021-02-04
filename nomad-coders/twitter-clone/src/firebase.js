39b9c087446f38aac7fd939dbad01f24d570ec6e
eunbin-lee <euns2bin@gmail.com>
2021-02-04 16:53:49 +0900

    1.0 Initialization of cloning twitter

---
+++ nomad-coders/twitter-clone/src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyD_ZCbtgI2IvkgE7GYr_x5ebip0COo0-UQ',
+  authDomain: 'twitter-clone-f053d.firebaseapp.com',
+  projectId: 'twitter-clone-f053d',
+  storageBucket: 'twitter-clone-f053d.appspot.com',
+  messagingSenderId: '169521941554',
+  appId: '1:169521941554:web:87311b8fec13f63907c02b',
+};
+
+export default firebase.initializeApp(firebaseConfig);
