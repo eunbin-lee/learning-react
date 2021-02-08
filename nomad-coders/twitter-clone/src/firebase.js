15388a812e48c0802de46b4f05a39e6f5f083779
eunbin-lee <euns2bin@gmail.com>
2021-02-08 17:01:02 +0900

    1.2 Router Setup

--- nomad-coders/twitter-clone/src/firebase.js
+++ nomad-coders/twitter-clone/src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyD_ZCbtgI2IvkgE7GYr_x5ebip0COo0-UQ',
-  authDomain: 'twitter-clone-f053d.firebaseapp.com',
-  projectId: 'twitter-clone-f053d',
-  storageBucket: 'twitter-clone-f053d.appspot.com',
-  messagingSenderId: '169521941554',
-  appId: '1:169521941554:web:87311b8fec13f63907c02b',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGEIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 export default firebase.initializeApp(firebaseConfig);
